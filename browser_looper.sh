87cc410aba1687e748c4ca858883aa0de5cb2090
Robert Focke <robert_focke@yahoo.com>
2016-04-22 13:21:41 +0000

    working version for HomeToGo

--- browser_looper.sh
+++ browser_looper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # initial setup
 set -euo pipefail
@@ -17,88 +17,79 @@ IFS=$'\n\t'
 URLs=(
   #de
   'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w76390421p78982821/'
-  #at
-  'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w99569449p103509175/'
-  #dashboard
-  'https://sites.google.com/a/hometogo.de/analytics-dashboard/'
-  #ch
-  'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w99573622p103478678/'
   #fr
   'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w94899173p98926757/'
   #dashboard
-  'https://sites.google.com/a/hometogo.de/analytics-dashboard/'
+  'https://sites.google.com/a/hometogo.de/raspberrypi/organic-search'
   #nl
   'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w96410452p100559082/'
   #pl
   'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w95347242p99408504/'
   #dashboard
-  'https://sites.google.com/a/hometogo.de/analytics-dashboard/'
+  'https://sites.google.com/a/hometogo.de/raspberrypi/dashboard-users'
   #uk
   'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w99535565p103509174/'
   #it
   'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w99824667p103742976/'
   #dashboard
-  'https://sites.google.com/a/hometogo.de/analytics-dashboard/'
+  'https://sites.google.com/a/hometogo.de/raspberrypi/organic-search'
   #es
   'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w100636822p104521485/'
   #com
   'https://analytics.google.com/analytics/web/#realtime/rt-overview/a45666214w100636822p104521485/'
   #dashboard
-  'https://sites.google.com/a/hometogo.de/analytics-dashboard/'
+  'https://sites.google.com/a/hometogo.de/raspberrypi/dashboard-users'
 )
 
 # Array containing the tags for the various URLS
 TAGS=(
   'DE'
-  'AT'
-  'DASH'
-  'CH'
   'FR'
-  'DASH'
+  ' '
   'NL'
   'PL'
-  'DASH'
+  ' '
   'UK'
   'IT'
-  'DASH'
+  ' '
   'ES'
   'COM'
-  'DASH'
+  ' '
 )
 
 # set this variable to the number of times you want to loop, 0 for infinite
-repetitions=3
+repetitions=0
 
 # these variables represent the number of seconds each URL will be displayed for
 default_sleep=60
 long_sleep=180
 
 # every multiple of long in the URL array will be displayed longer, 0 = all displayed equally
-long=0
+long=3
 
 
 function OPEN_URL {
     chromium-browser --no-first-run --noerrdialogs --kiosk $1 &
     sleep $2
-    /usr/bin/pkill --oldest --signal TERM -f chromium-browser
+    pkill --oldest --signal TERM -f chromium-browser
 }
 
 function SQUARE {
-    python main.py $1 &
+    python2 /home/pi/.config/lxsession/LXDE-pi/main.py $1 &
 }
 
 function UNSQUARE {
-  pkill -f main.py
+    pkill -f main.py
 }
 
 # check if loop should be finite or infinite
 if [ $repetitions -eq 0 ]
 then
     # infinite loop section
-    counter=1
     while true
     do
         arraycounter=0
+	counter=1
         for URL in "${URLs[@]}"
         do
             # check if some URLs should be displayed longer
@@ -120,6 +111,7 @@ then
               UNSQUARE
             fi
             arraycounter=$((arraycounter+1))
+	    counter=$((counter+1))
         done
     done
 else
